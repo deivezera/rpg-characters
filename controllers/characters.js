b05f699175bde5c11d64b19cabe5a99246336f71
deivezera <dzabroski@gmail.com>
2025-02-01 18:47:44 -0300

    added try catch

--- controllers/characters.js
+++ controllers/characters.js
@@ -4,90 +4,111 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
   //#swagger.tags=['Characters']
-  const result = await mongodb.getDatabase().db().collection('characters').find();
-  result.toArray()
-    .then((characters) => {
-      if(!characters.length){
-        return next(new AppError('Character not found', 404));
-      }
-      res.setHeader('Contenct-Type', 'application/json');
-      res.status(200).json(characters);
-    })
+  try{
+    const result = await mongodb.getDatabase().db().collection('characters').find();
+    result.toArray()
+      .then((characters) => {
+        if(!characters.length){
+          return next(new AppError('Character not found', 404));
+        }
+        res.setHeader('Contenct-Type', 'application/json');
+        res.status(200).json(characters);
+      })
+  } catch(err){
+    next(err)
+  }
 }
 
 const getSingle = async (req,res, next) => {
   //#swagger.tags=['Characters']
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDatabase().db().collection('characters').find({ _id: userId });
-  result.toArray()
-    .then((characters) => {
-      if(!characters.length){
-        return next(new AppError('Character not found', 404));
-      }
-      res.setHeader('Contenct-Type', 'application/json');
-      res.status(200).json(characters[0]);
-    })
+  try{
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('characters').find({ _id: userId });
+    result.toArray()
+      .then((characters) => {
+        if(!characters.length){
+          return next(new AppError('Character not found', 404));
+        }
+        res.setHeader('Contenct-Type', 'application/json');
+        res.status(200).json(characters[0]);
+      })
+  } catch(err){
+    next(err)
+  }
 }
 
 const createCharacter = async (req,res, next) => {
   //#swagger.tags=['Characters']
-  const { name, race, classname, appearance, originStory, goals, personalityTraits, weaknesses } = req.body;
-  const character = {
-    name,
-    race,
-    classname,
-    appearance,
-    originStory,
-    goals,
-    personalityTraits,
-    weaknesses
-  }
-
-  const response = await mongodb.getDatabase().db().collection('characters').insertOne(character);
-  if(response.acknowledged > 0) {
-    res.status(204).send();
-  } else {
-    return next(new AppError(response.error || 'Some error occurred while creating character', 500));
+  try{
+    const { name, race, classname, appearance, originStory, goals, personalityTraits, weaknesses } = req.body;
+    const character = {
+      name,
+      race,
+      classname,
+      appearance,
+      originStory,
+      goals,
+      personalityTraits,
+      weaknesses
+    }
+  
+    const response = await mongodb.getDatabase().db().collection('characters').insertOne(character);
+    if(response.acknowledged > 0) {
+      res.status(204).send();
+    } else {
+      return next(new AppError(response.error || 'Some error occurred while creating character', 500));
+    }
+  } catch(err){
+    next(err)
   }
 }
 
 const updateCharacter = async (req,res, next) => {
   //#swagger.tags=['Characters']
-  if (!ObjectId.isValid(req.params.id)) {
-    return next(new AppError('Must use a valid character id to update a character.', 400));
-  }
-  const characterId = new ObjectId(req.params.id);
-  const { name, race, classname, appearance, originStory, goals, personalityTraits, weaknesses } = req.body;
-  const character = {
-    name,
-    race,
-    classname,
-    appearance,
-    originStory,
-    goals,
-    personalityTraits,
-    weaknesses
-  }
 
-  const response = await mongodb.getDatabase().db().collection('characters').replaceOne({_id: characterId }, character);
-  if(response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    return next(new AppError(response.error || 'Some error occurred while creating character', 500));
+  try{
+    if (!ObjectId.isValid(req.params.id)) {
+      return next(new AppError('Must use a valid character id to update a character.', 400));
+    }
+    const characterId = new ObjectId(req.params.id);
+    const { name, race, classname, appearance, originStory, goals, personalityTraits, weaknesses } = req.body;
+    const character = {
+      name,
+      race,
+      classname,
+      appearance,
+      originStory,
+      goals,
+      personalityTraits,
+      weaknesses
+    }
+  
+    const response = await mongodb.getDatabase().db().collection('characters').replaceOne({_id: characterId }, character);
+    if(response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      return next(new AppError(response.error || 'Some error occurred while creating character', 500));
+    }
+  } catch(err){
+    next(err)
   }
 }
 
 const deleteCharacter = async (req,res, next) => {
   //#swagger.tags=['Characters']
-  if (!ObjectId.isValid(req.params.id)) {
-    return next(new AppError('Must use a valid character id to delete a character.', 400));
-  }
-  const characterId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db().collection('characters').deleteOne({_id: characterId } , true);
-  if(response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    return next(new AppError(response.error || 'Some error occurred while creating character', 500));
+  try{
+    if (!ObjectId.isValid(req.params.id)) {
+      return next(new AppError('Must use a valid character id to delete a character.', 400));
+    }
+    const characterId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('characters').deleteOne({_id: characterId } , true);
+    if(response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      return next(new AppError(response.error || 'Some error occurred while creating character', 500));
+    }
+  } catch(err){
+    next(err)
   }
 }
 
--- controllers/histories.js
+++ controllers/histories.js
@@ -4,84 +4,105 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
   //#swagger.tags=['Histories']
-  const result = await mongodb.getDatabase().db().collection('histories').find();
-  result.toArray()
-    .then((histories) => {
-      if(!histories.length){
-        return next(new AppError('history not found', 404));
-      }
-      res.setHeader('Contenct-Type', 'application/json');
-      res.status(200).json(histories);
-    })
+
+  try{
+    const result = await mongodb.getDatabase().db().collection('histories').find();
+    result.toArray()
+      .then((histories) => {
+        if(histories.length){
+          return next(new AppError('history not found', 404));
+        }
+        res.setHeader('Contenct-Type', 'application/json');
+        res.status(200).json(histories);
+      })
+  }catch(err){
+    next(err)
+  }
 }
 
 const getSingle = async (req,res, next) => {
   //#swagger.tags=['Histories']
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDatabase().db().collection('histories').find({ _id: userId });
-  result.toArray()
-    .then((histories) => {
-      if(!histories.length){
-        return next(new AppError('History not found', 404));
-      }
-      res.setHeader('Contenct-Type', 'application/json');
-      res.status(200).json(histories[0]);
-    })
+  try{
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('histories').find({ _id: userId });
+    result.toArray()
+      .then((histories) => {
+        if(!histories.length){
+          return next(new AppError('History not found', 404));
+        }
+        res.setHeader('Contenct-Type', 'application/json');
+        res.status(200).json(histories[0]);
+      })
+  }catch(err){
+    next(err)
+  }
 }
 
 const createHistory = async (req,res, next) => {
   //#swagger.tags=['Histories']
-  const { title, intro, description, world_description, challenge } = req.body;
-  const history = {
-    title,
-    intro,
-    description,
-    world_description,
-    challenge
-  }
+  try{
+    const { title, intro, description, world_description, challenge } = req.body;
+    const history = {
+      title,
+      intro,
+      description,
+      world_description,
+      challenge
+    }
 
-  const response = await mongodb.getDatabase().db().collection('histories').insertOne(history);
-  if(response.acknowledged > 0) {
-    res.status(204).send();
-  } else {
-    return next(new AppError(response.error || 'Some error occurred while creating your histories', 500));
+    const response = await mongodb.getDatabase().db().collection('histories').insertOne(history);
+    if(response.acknowledged > 0) {
+      res.status(204).send();
+    } else {
+      return next(new AppError(response.error || 'Some error occurred while creating your histories', 500));
+    }
+  }catch(err){
+    next(err)
   }
 }
 
 const updateHistory = async (req,res, next) => {
   //#swagger.tags=['Histories']
-  if (!ObjectId.isValid(req.params.id)) {
-    return next(new AppError('Must use a valid history id to update a history.', 400));
-  }
-  const historyId = new ObjectId(req.params.id);
-  const { title, intro, description, world_description, challenge } = req.body;
-  const history = {
-    title,
-    intro,
-    description,
-    world_description,
-    challenge
-  }
-
-  const response = await mongodb.getDatabase().db().collection('histories').replaceOne({_id: historyId }, history);
-  if(response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    return next(new AppError(response.error || 'Some error occurred while updating your history', 500));
+  try{
+    if (!ObjectId.isValid(req.params.id)) {
+      return next(new AppError('Must use a valid history id to update a history.', 400));
+    }
+    const historyId = new ObjectId(req.params.id);
+    const { title, intro, description, world_description, challenge } = req.body;
+    const history = {
+      title,
+      intro,
+      description,
+      world_description,
+      challenge
+    }
+  
+    const response = await mongodb.getDatabase().db().collection('histories').replaceOne({_id: historyId }, history);
+    if(response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      return next(new AppError(response.error || 'Some error occurred while updating your history', 500));
+    }
+  }catch(err){
+    next(err)
   }
 }
 
 const deleteHistory = async (req,res, next) => {
   //#swagger.tags=['Histories']
-  if (!ObjectId.isValid(req.params.id)) {
-    return next(new AppError('Must use a valid history id to delete a history.', 400));
-  }
-  const historyId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db().collection('histories').deleteOne({_id: historyId } , true);
-  if(response.deletedCount > 0) {
-    res.status(204).send();
-  } else {
-    return next(new AppError(response.error || 'Some error occurred while creating history', 500));
+  try{
+    if (!ObjectId.isValid(req.params.id)) {
+      return next(new AppError('Must use a valid history id to delete a history.', 400));
+    }
+    const historyId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('histories').deleteOne({_id: historyId } , true);
+    if(response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      return next(new AppError(response.error || 'Some error occurred while creating history', 500));
+    }
+  } catch(err){
+    next(err)
   }
 }
 
